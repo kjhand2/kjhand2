9beab1bae1c21c784f29ed07f2b0e383024fff02
root <root@localhost.localdomain>
2017-10-27 15:17:06 -0500

    some stuff

--- mp3/3.2.2.py
+++ mp3/3.2.2.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python2.7
 import dpkt
 import sys
+from collections import defaultdict
+
+d=defaultdict(list)
 
 
-def sanity_check():
-    if (len(sys.argv) < 2):
-        print "error: need argument"
-        sys.exit(1)
+if (len(sys.argv) < 2):
+	print "error: need argument"
+	sys.exit(1)
 
-    filename = sys.argv[1]
-    return
+filename = sys.argv[1]
 
 
-if __name__ == '__main__':
-    sanity_check()
+f = open(filename)
+pcap = dpkt.pcap.Reader(f)
 
-print "everything is fine"
+for ts, buf in pcap:
+	if(buf != 0):
+		eth = dpkt.ethernet.Ethernet(buf)
+	if(eth != 0):
+		ip = eth.data
+	if(ip != 0):
+		tcp = ip.data
+print(tcp.flags)
+	# syn_flag = ( tcp.flags & dpkt.tcp.TH_SYN )# | (dpkt.udp.flags(buf) & dpkt.udp.TH_SYN)
+	# ack_flag = ( tcp.flags & dpkt.tcp.TH_ACK )# | (dpkt.udp.flags(buf) & dpkt.udp.TH_ACK)
