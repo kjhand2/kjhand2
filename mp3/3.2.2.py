669a9c5a4c329647f968a03f7cdcfe45a5538d3e
root <kjhand2@illinois.edu>
2017-11-04 22:01:52 +0000

    updates

--- mp3/3.2.2.py
+++ mp3/3.2.2.py
@@ -21,7 +21,7 @@ if (len(sys.argv) < 2):
 filename = sys.argv[1]
 
 
-f = open(filename)
+f = open(filename, "rb")
 pcap = dpkt.pcap.Reader(f)
 
 
@@ -37,31 +37,32 @@ for ts, buf in pcap:
 				tcp = ip.data
 				print(4)
 				print(tcp.flags & dpkt.tcp.TH_ACK)
-				sys.exit(1)
+				# sys.exit(1)
 				# test for ACK-SYN
 				if (tcp.flags & dpkt.tcp.TH_ACK) != 0 and (tcp.flags & dpkt.tcp.TH_SYN) != 0:
 					print ("made it to ack")
 					if ip.dst not in s_a:
 						s_a[ip.dst] = 1
+						if ip.dst not in ip_list:
+							ip_list.append(ip.dst)
 					else:
 						s_a[ip.dst] += 1
-# # add a zero if we find a ack-syn before a syn
-# 		if ip.dst not in s:
-# 			s[ip.dst] = 0
-# # test for SYN
-# 	elif (tcp.flags & dpkt.tcp.TH_SYN) != 0:
-# 		print ("made it to syn")
-# 		if ip.dst not in s:
-# 			s[ip.dst] = 1
-# 		else:
-# 			s[ip.dst] += 1
-# # add a zero if we find a syn before a ack-syn
-# 		if ip.dst not in s_a:
-# 			s_a[ip.dst] = 0
-# #append ip addresses to ip_lists for each iteration
-# 	if ip.dst not in ip_list:
-# 		ip_list.append(ip.dst)
-# 		ip_list2.append(ip.src)
+			# add a zero if we find a ack-syn before a syn
+					if ip.dst not in s:
+						s[ip.dst] = 0
+			# test for SYN
+				elif (tcp.flags & dpkt.tcp.TH_SYN) != 0:
+					print ("made it to syn")
+					if ip.src not in s:
+						s[ip.src] = 1
+						if ip.src not in ip_list:
+							ip_list.append(ip.src)
+					else:
+						s[ip.src] += 1
+			# add a zero if we find a syn before a ack-syn
+					if ip.dst not in s_a:
+						s_a[ip.src] = 0
+
 # #run through list to find the IP address that works
 # x=0
 # for c_IP in ip_list:
